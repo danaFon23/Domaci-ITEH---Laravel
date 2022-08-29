6906fa9afe3e087c3f4b0b7fbd7eea2ab738a541
danaFon23 <vukotic.danica95@gmail.com>
2022-08-29 15:03:05 +0200

    Naravljeni Modeli i Migracije za kreiranje Modela

---
+++ laravel-domaci/database/migrations/2022_08_29_122701_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('duration');
+            $table->string('year');
+            $table->foreignId('director_id');
+            $table->string('genre_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
