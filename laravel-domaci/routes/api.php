55250fbc784887504c23f5d13a11ac7265057d74
danaFon23 <vukotic.danica95@gmail.com>
2022-08-30 23:49:44 +0200

    Kreirane api rute i kontroleri

--- laravel-domaci/routes/api.php
+++ laravel-domaci/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DirectorController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Route::get('director', [DirectorController::class, 'index']);
+
+Route::resource('directors', DirectorController::class);
+
+Route::resource('movies', MovieController::class);
